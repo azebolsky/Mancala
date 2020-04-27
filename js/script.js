06851be2d581079c54c1f872685b48adee024255
ADAM <zebolsky.adam@gmail.com>
2020-04-27 09:14:26 -0500

    update media queries

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 const KEY = {
     '1': 'player 1',
     '-1': 'player 2',
-};  
-const MARBLE_SRC = 'https://i.imgur.com/bwgAbPE.png'; 
+};
+const MARBLE_SRC = 'https://i.imgur.com/bwgAbPE.png';
 
 /*----- **************** app's state (variables) **************** -----*/
 let turn;
@@ -27,21 +27,24 @@ document.getElementById('restart-btn').addEventListener('click', init);
 /*----- functions -----*/
 init();
 
+// my pride and joy function below 
 function podClick(evt) {
     // shows corresponding id that was clicked
     let podId = evt.target.id;
     let persistTurn = checkLastMarbleInMancala(podId, board[podId])
     let correspondingPodMarbles = checkLastMarbleToOpponentPod(podId, board[podId])
-    if(!podId || winner) return;
+    if (!podId || winner) return;
     let curIndex = podId;
     let curPodCount = board[podId];
     for (let i = 0; i < board[podId]; i++) {
+        // If player 1's turn and they have enough marbles to reach player 2's mancala, skip over p2's mancala and go to pod 0
         if (turn === 1) {
             if (curIndex === 13) {
                 curIndex = 0;
                 curPodCount--;
             }
         }
+        // If player 2's turn and they have enough marbles to reach player 1's mancala, skip over p1's mancala and go to pod 7
         if (turn === -1) {
             if (curIndex === 6) {
                 curIndex = 7;
@@ -62,7 +65,7 @@ function podClick(evt) {
     board[podId] = 0;
     correspondingPodMarbles;
     if (!persistTurn) {
-        turn*=-1;
+        turn *= -1;
     }
     winner = checkWinner();
     render();
@@ -91,13 +94,13 @@ function render() {
 function winnerMessage() {
     if (!winner) {
         messageEl.textContent = `It's ${KEY[turn]}'s turn!`;
-    } 
+    }
     if (winner && (board[6] === board[13])) {
         messageEl.textContent = 'Looks like we have a tie! Play again and settle the score...';
-    } 
-    if (winner && (board[6] > board[13])){
+    }
+    if (winner && (board[6] > board[13])) {
         messageEl.textContent = 'Player 1 is the winner!';
-    } 
+    }
     if (winner && (board[13] > board[6])) {
         messageEl.textContent = 'Player 2 is the winner!';
     }
@@ -105,13 +108,13 @@ function winnerMessage() {
 
 function checkWinner() {
     if (board[0] === 0 && board[1] === 0 && board[2] === 0 && board[3] === 0 && board[4] === 0 && board[5] === 0) {
-        if(board[6] > board[13]) {
+        if (board[6] > board[13]) {
             return 1;
         } else {
             return -1;
         }
     } else if (board[7] === 0 && board[8] === 0 && board[9] === 0 && board[10] === 0 && board[11] === 0 && board[12] === 0) {
-        if(board[6] > board[13]) {
+        if (board[6] > board[13]) {
             return 1;
         } else {
             return -1;
@@ -124,22 +127,22 @@ function checkWinner() {
 function turnBlock() {
     // prevent turn 1 (player 1) from clicking on turn -1's (player 2) side and viceversa
     if (turn === 1) {
-        playerTwoTurn.forEach(function(p2) {
+        playerTwoTurn.forEach(function (p2) {
             p2.style.pointerEvents = 'none';
         });
-        playerOneTurn.forEach(function(p1) {
+        playerOneTurn.forEach(function (p1) {
             p1.style.pointerEvents = '';
         });
         for (let i = 0; i < (board.length - 8); i++) {
             if (board[i] === 0) {
                 document.querySelector(`.a${i}`).style.pointerEvents = 'none';
-            } 
+            }
         };
     } else if (turn === -1) {
-        playerOneTurn.forEach(function(p1) {
+        playerOneTurn.forEach(function (p1) {
             p1.style.pointerEvents = 'none';
         });
-        playerTwoTurn.forEach(function(p2) {
+        playerTwoTurn.forEach(function (p2) {
             p2.style.pointerEvents = '';
         });
         for (let i = 7; i < (board.length - 1); i++) {
@@ -156,7 +159,7 @@ function scoreBoard() {
 }
 
 function checkLastMarbleInMancala(position, val) {
-    while(val !== 0) {
+    while (val !== 0) {
         val--
         position++
     }
@@ -165,33 +168,33 @@ function checkLastMarbleInMancala(position, val) {
 
 function checkLastMarbleToOpponentPod(position, val) {
     if (turn === 1) {
-        while(val !== 0) {
+        while (val !== 0) {
             val--
             position++
         }
-        if ((board[position] === 0) && (position !== 6) && (position !== 7) && (position !== 8) && (position !== 9) 
-                                    && (position !== 10) && (position !== 11) && (position !== 12) && (position !== 13)) {
+        if ((board[position] === 0) && (position !== 6) && (position !== 7) && (position !== 8) && (position !== 9)
+            && (position !== 10) && (position !== 11) && (position !== 12) && (position !== 13)) {
             let correspondingPod = 12 - position;
             if (board[correspondingPod] > 0) {
-                board[position]-=1;
-                board[6]+=board[correspondingPod];
-                board[6]+=1;
+                board[position] -= 1;
+                board[6] += board[correspondingPod];
+                board[6] += 1;
                 board[correspondingPod] = 0;
             }
         }
     }
     if (turn === -1) {
-        while(val !== 0) {
+        while (val !== 0) {
             val--
             position++
         }
         if ((board[position] === 0) && (position !== 13) && (position !== 0) && (position !== 1) && (position !== 2)
-                                     && (position !== 3) && (position !== 4) && (position !== 5) && (position !== 6)) {
+            && (position !== 3) && (position !== 4) && (position !== 5) && (position !== 6)) {
             let correspondingPod = 12 - position;
             if (board[correspondingPod] > 0) {
-                board[position]-=1;
-                board[13]+=board[correspondingPod];
-                board[13]+=1;
+                board[position] -= 1;
+                board[13] += board[correspondingPod];
+                board[13] += 1;
                 board[correspondingPod] = 0;
             }
         }
